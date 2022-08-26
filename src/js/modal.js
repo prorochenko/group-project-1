edb99d3b996b143658753eae127012720f5999d7
Anna Karachkovska <akarachkovska@gmail.com>
2022-08-26 16:34:19 +0300

    js update

--- src/js/modal.js
+++ src/js/modal.js
@@ -4,10 +4,8 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
-​
   refs.openModalBtns.forEach(btn => btn.addEventListener('click', toggleModal));
   refs.closeModalBtn.addEventListener('click', toggleModal);
-​
   function toggleModal() {
     refs.modal.classList.toggle('backdrop--hidden');
   }
